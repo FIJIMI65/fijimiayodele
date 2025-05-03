7d5d6a13d1e5b0309e8af01ed65db8ea99db4ad9
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-05-03 08:30:40 +0100

    app's main script

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+// import './css/bootstrap-theme.css';
+// import './css/bootstrap-theme.min.css';
+
+// import './css/bootstrap.min.css';
+import './css/templatemo-style.css';
+import './css/bootstrap.css';
+import { Home } from './components/Home';
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
