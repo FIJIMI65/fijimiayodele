5ad4668452a9ebe3ed11fc56929dc9f8dc9714b3
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-05-12 13:41:45 +0100

    fixed Projects.js: improved code rendering including project details(thumbnails, links, etc), added feature to dynamically add/remove projects and rendering them horizontally (scrollable) for pc and vertically (scrollable) for mobile devices

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,189 @@
+import { useRef, useState, useEffect } from "react"
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 import projectItem1 from "../img/project-item-01.jpg"
 import projectItem2 from "../img/project-item-02.jpg"
 import projectItem3 from "../img/project-item-03.jpg"
+import { useIsMobile } from "../hooks/IsMobile"
+
+const arrowSize = 55
+const descriptionLength = 9
+const projectArray = [
+	{
+		thumbnail: projectItem1,
+		alt: "project",
+		title: "Best Design",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem2,
+		alt: "project",
+		title: "Creative Pen",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem3,
+		alt: "project",
+		title: "Nice Capture",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem1,
+		alt: "project",
+		title: "Best Design",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem2,
+		alt: "project",
+		title: "Creative Pen",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem3,
+		alt: "project",
+		title: "Nice Capture",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem1,
+		alt: "project",
+		title: "Best Design",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem2,
+		alt: "project",
+		title: "Creative Pen",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem3,
+		alt: "project",
+		title: "Nice Capture",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem3,
+		alt: "project",
+		title: "Nice Capture",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem2,
+		alt: "project",
+		title: "Creative Pen",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem3,
+		alt: "project",
+		title: "Nice Capture",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem1,
+		alt: "project",
+		title: "Best Design",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem2,
+		alt: "project",
+		title: "Creative Pen",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem3,
+		alt: "project",
+		title: "Nice Capture",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+	{
+		thumbnail: projectItem3,
+		alt: "project",
+		title: "Nice Capture",
+		description: "Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.",
+		link: "#",
+		buttonText: "More",
+	},
+]
 
 function Projects () {
+	const isMobile = useIsMobile();
+	console.log('isMobile:', isMobile)
+	const containerRef = useRef(null)
+	const [leftHoverColor, setLeftHoverColor] = useState('#8d8989')
+	const [rightHoverColor, setRightHoverColor] = useState('#8d8989')
+	const [atStart, setAtStart] = useState(true);
+	const [atEnd, setAtEnd] = useState(false);
+	
+	const chunkedProjects = [];
+	for (let i = 0; i < projectArray.length; i += 6) {
+		chunkedProjects.push(projectArray.slice(i, i + 6));
+	}
+	const checkScrollPosition = () => {
+		const scrollPosition = containerRef.current;
+		if (!scrollPosition) return;
+		setAtStart(scrollPosition.scrollLeft === 0);
+		setAtEnd(scrollPosition.scrollLeft + scrollPosition.offsetWidth >= scrollPosition.scrollWidth - 1);
+	};
+	useEffect(() => {
+		const scrollPosition = containerRef.current;
+		if (!scrollPosition) return;
+
+		// Initial check
+		checkScrollPosition();
+
+		// Attach scroll listener
+		scrollPosition.addEventListener('scroll', checkScrollPosition);
+		return () => scrollPosition.removeEventListener('scroll', checkScrollPosition);
+	}, []);
+
+	const scrollHandler = (direction) => {
+		if (containerRef.current) {
+			containerRef.current.scrollBy({ left: direction==='left'?-containerRef.current.offsetWidth:containerRef.current.offsetWidth, behavior: 'smooth' });
+		}
+	};
+	const hoverHandler = (action='') => {
+		if (action === 'left') {
+			setLeftHoverColor('#1f2327');
+		} else if (action === 'right') {
+			setRightHoverColor('#1f2327');
+		} else {
+			setLeftHoverColor('#8d8989');
+			setRightHoverColor('#8d8989');
+		}
+	}
 	return (
 		<>
 			{/* projects section */}
@@ -18,42 +199,53 @@ function Projects () {
 										</div>
 										<div className="row">
 											<div className="col-md-12">
-												<div className="owl-carousel owl-theme projects-container">
-													<div>
-														<div className="project-item">
-															<a href="../img/project-item-01.jpg" data-lightbox="image-1"><img src={projectItem1} alt=""/></a>
-															<div className="text-content">
-																<h4>Best Design</h4>
-																<p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>
-																<div className="primary-button">
-																	<a href="/">Discover More</a>
+												<div style={{display: 'flex', flexDirection: 'column'}}>
+													<div style={{ position: 'relative' }}>
+														{/* Left Arrow */}
+														{!atStart ?
+														<div
+														onClick={() => scrollHandler('left')}
+														onMouseEnter={() => hoverHandler('left')}
+														onMouseLeave={() => hoverHandler('')}
+														className="project_arrow arrow_left">
+															<FaChevronLeft size={arrowSize} color={leftHoverColor} />
+														</div>:null}
+														<div ref={containerRef} className="owl-carousel owl-theme projectContainer">
+														{chunkedProjects.map((group, i) => (
+															<div key={i} className="projectPage">
+															{group.map((project, index) => (
+																<div key={index} className="project-item">
+																	<a href={project.link} data-lightbox="image-1">
+																		<img src={project.thumbnail} alt={`${project.alt}-${index + 1}`} />
+																	</a>
+																	<div className="text-content">
+																		<h4 className="project_h4">{project.title} {index + 1}</h4>
+																		<p className="project_paragraph">
+																			{isMobile
+																			? (project.description.split(' ').length > descriptionLength
+																				? project.description.split(' ').slice(0, descriptionLength).join(' ') + '...'
+																				: project.description)
+																			: project.description}
+																		</p>
+																		<div className="primary-button">
+																			<a href={project.link}>{project.buttonText}</a>
+																		</div>
+																	</div>
 																</div>
+															))}
 															</div>
+														))}
 														</div>
-													</div>
-													<div>
-														<div className="project-item">
-															<a href="../img/project-item-02.jpg" data-lightbox="image-1"><img src={projectItem2} alt=""/></a>
-															<div className="text-content">
-																<h4>Creative Pen</h4>
-																<p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>
-																<div className="primary-button">
-																	<a href="/">Discover More</a>
-																</div>
-															</div>
-														</div>
-													</div>
-													<div>
-														<div className="project-item">
-															<a href="../img/project-item-03.jpg" data-lightbox="image-1"><img src={projectItem3} alt=""/></a>
-															<div className="text-content">
-																<h4>Nice Capture</h4>
-																<p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>
-																<div className="primary-button">
-																	<a href="/">Discover More</a>
-																</div>
-															</div>
-														</div>
+														{/* Right Arrow */}
+														{!atEnd ?
+														<div
+														onClick={() => scrollHandler('right')}
+														onMouseEnter={() => hoverHandler('right')}
+														onMouseLeave={() => hoverHandler('')}
+														className="project_arrow arrow_right"
+														>
+															<FaChevronRight size={arrowSize} color={rightHoverColor} />
+														</div>:null}
 													</div>
 												</div>
 											</div>
