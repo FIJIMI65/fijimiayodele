23138a17efae3e54bc41990ec3a0adacb4d04ab4
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-06-13 23:37:49 +0100

    App.js: imported and passed the useSetHeadTag() which handles setting site logo dynamically in development mode and runs only on first mount

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import './css/styles.css';
 import './css/bootstrap.css';
 import { Index } from './components/Index';
+import { useSetHeadTag } from './hooks/useSetHeadTag';
 
 function App() {
+  useSetHeadTag();
   return (
     <>
       <Index />
