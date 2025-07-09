7d00054c5d7bd0c48a015a73349aec7cbae75668
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-09 15:24:42 +0100

    Contact.js: fixed up inputs and textarea to take and send messages to the site owner and confirmation to the visitor

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,73 @@
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import { toast } from 'react-toastify';
 import dafetite from "../img/dafelogoWhiteTransparent.png"
 import { DateHook } from "../hooks/DateHook";
 import { useIsMobile } from "../hooks/IsMobile";
 import { Entry } from "../entry/entry";
 
+const formInputValues = [
+	{
+		type: "text",
+		name: "name",
+		id: "name",
+		placeholder: "Your Name",
+	},
+	{
+		type: "email",
+		name: "email",
+		id: "email",
+		placeholder: "Email",
+	}
+]
+const formValues = {
+	name: "",
+	email: "",
+	message: ""
+}
 function Contact () {
 	const { tangline } = Entry();
 	const isMobile = useIsMobile();
 	const year = DateHook().todayYear;
+	const [isSending, setIsSending] = useState(false);
+	const [formData, setFormData] = useState(formValues);
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value
+		});
+	}
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setIsSending(true);
+
+		// First, send the message youself, site owner (you)
+		emailjs.send(
+			'service_2x0u2dm',      // your service ID
+			'template_ddksigd',     // Template A - message to you
+			formData,
+			'qQE_DwjBJWwq6sEOg'     // your public key
+			).then(() => {
+				// Then send acknowledgment to the sender
+				emailjs.send(
+					'service_2x0u2dm',    // same service
+					'template_5w7eds8',   // Template B - thank-you to user
+					formData,
+					'qQE_DwjBJWwq6sEOg'   // your public key
+				).then(() => {
+					toast.success('Message sent successfully!');
+					setFormData(formValues);
+				}).catch((error) => {
+					toast.error(`Failed: ${error.text}`);
+				});
+
+			}).catch((error) => {
+				alert('Failed to send message: ' + error.text);
+			}).finally(() => {
+				setIsSending(false);
+			});
+	};
 	return (
 		<>
 			{/* contact section */}
@@ -23,18 +84,42 @@ function Contact () {
 										<div className="row">
 											<div className="col-md-8">
 												<div className="contat-form">
-													<form id="contact" action="" method="post">
+													<form onSubmit={handleSubmit}>
 														<fieldset>
-															<input name="name" type="text" className="form-control" id="name" placeholder="Your Name" required />
+															{formInputValues.map((input, index) => (
+																<input
+																	key={index}
+																	type={input.type}
+																	name={input.name}
+																	className="form-control"
+																	id={input.id}
+																	placeholder={input.placeholder}
+																	value={formData[input.name]}
+																	onChange={handleInputChange}
+																	required
+																/>
+															))}
 														</fieldset>
 														<fieldset>
-															<input name="email" type="email" className="form-control" id="email" placeholder="Email" required />
+															<textarea
+															name="message"
+															rows="6"
+															className="form-control"
+															id="message"
+															placeholder="Message"
+															value={formData.message}
+															onChange={handleInputChange}
+															required
+															></textarea>
 														</fieldset>
 														<fieldset>
-															<textarea name="message" rows="6" className="form-control" id="message" placeholder="Message" required></textarea>
-														</fieldset>
-														<fieldset>
-															<button type="submit" id="form-submit" className="btn">Send Message</button>
+															<button
+															type="submit"
+															id="form-submit"
+															className="btn"
+															disabled={isSending}>
+																{isSending ? "Sending..." : "Send Message"}
+															</button>
 														</fieldset>
 													</form>
 												</div>
@@ -52,7 +137,7 @@ function Contact () {
 								<div className="col-md-12">
 									<div className="footer">
 										<p><span style={!isMobile?styles.footer : {}}>
-										Copyright &copy; {year} | Developed by 
+										Copyright &copy; {year} | Developed by
 											<a
 											target="_blank"
 											rel="noopener noreferrer"
