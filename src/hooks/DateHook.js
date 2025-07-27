531238e903e266a651da61fcbbeaa2342098c2b6
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-27 23:03:07 +0100

    DateHook.js: added todayDateWithSuffix, todayMonthName,

--- src/hooks/DateHook.js
+++ src/hooks/DateHook.js
@@ -1,9 +1,18 @@
 
 
 function DateHook(date=new Date()) {
-	const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat'];
+	const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	const daySuffixes = ['th', 'st', 'nd', 'rd'];
+	const getDaySuffix = (day) => {
+		const suffixIndex = (day % 10 < 4 && Math.floor(day / 10) !== 1) ? day % 10 : 0;
+		return daySuffixes[suffixIndex];
+	};
+
 	const todayDate = date.getDate(); // 21
+	const todayDateWithSuffix = `${todayDate}${getDaySuffix(todayDate)}`; // '21st'
 	const todayMonth = date.getMonth() + 1; // 9
+	const todayMonthName = months[date.getMonth()]; // 'Sep'
 	const todayYear = date.getFullYear(); // 2021
 	const todayDay = days[date.getDay()]; // 'Monday'
 	const todayHours = date.getHours(); // 12
@@ -12,9 +21,11 @@ function DateHook(date=new Date()) {
 
 	return {
 		todayDate,
+		todayDateWithSuffix,
 		todayMonth,
 		todayYear,
 		todayDay,
+		todayMonthName,
 		todayHours,
 		todayMinutes,
 		todaySeconds
